08f1558416103fb9479952e0b2f80eac34abb5b0
darshitachaurasia <161128989+darshitachaurasia@users.noreply.github.com>
2024-08-12 23:47:13 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,29 @@
+/*import { Welcome } from '../components/Welcome/Welcome';
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+
+export default function HomePage() {
+  return (
+    <>
+      <Welcome />
+      <ColorSchemeToggle />
+    </>
+  );
+}*/
+
+import React from 'react';  // Add this line if you are getting the error
+import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorSchemeScript } from '@mantine/core';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <ColorSchemeScript />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
