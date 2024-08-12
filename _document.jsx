4325f9b2f0e50c27f3cd464fa3c596615b0cc135
darshitachaurasia <161128989+darshitachaurasia@users.noreply.github.com>
2024-08-12 23:46:58 +0530

    Add files via upload

---
+++ _document.jsx
@@ -0,0 +1,34 @@
+/*import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorSchemeScript } from '@mantine/core';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <ColorSchemeScript />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}*/
+
+import React from 'react';  // Add this line if you are getting the error
+import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorSchemeScript } from '@mantine/core';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <ColorSchemeScript />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
